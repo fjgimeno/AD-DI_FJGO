64028ac4fe32a902949ffbd8e1131efbbebc1b1d
ThinkpadX220 <javiergimeno1998ies@gmail.com>
2021-02-18 10:10:46 +0100

    Commit 1 DI

---
+++ project/client/src/pages/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="App logo"
+      src="~assets/shrek_beer.jpg"
+    >
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
